68dfa47b90ca96f828ef0dc7194a678ee067664f
ruixiaojia <ruixiaojia315@163.com>
2020-05-12 14:47:16 +0800

    add：Bundle分析工具

--- next.config.js
+++ next.config.js
@@ -2,18 +2,30 @@ const withCss = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass')
 const withTM = require('next-transpile-modules')(['antd']);
 const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
 
 module.exports = withPlugins(
   [
     withCss,
     withTM,
-    [withSass, {
-      cssModules: true,
-      cssLoaderOptions: {
-        localIdentName: '[path]___[local]___[hash:base64:5]',
+    [
+      withSass, {
+        cssModules: true,
+        cssLoaderOptions: {
+          localIdentName: '[path]___[local]___[hash:base64:5]',
+        },
       },
-    }, [PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD]],
+      [PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD]
+    ],
+    [
+      withBundleAnalyzer,
+      {
+        enabled: process.env.ANALYZE === 'true',
+      }
+    ],
   ],
   {
     webpack: (config) => {
