448791590e6a8c95d04577cd3498af2498232bd1
ruixiaojia <ruixiaojia315@163.com>
2020-07-16 17:03:50 +0800

    add: 背景图CRD

--- pages/settings/loginpage-setting/index.js
+++ pages/settings/loginpage-setting/index.js
@@ -1,83 +1,133 @@
 import React from 'react';
-import { Upload, Modal } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { Upload, Modal, Button, message, Spin } from 'antd';
+import { EyeOutlined, DeleteOutlined, ExclamationCircleOutlined, CloudUploadOutlined } from '@ant-design/icons';
+import JsCookies from "js-cookie";
 
-function getBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-  });
-}
+import { get, post } from '~/lib/io'
+
+import style from "./index.scss";
 
 class Home extends React.Component {
   constructor () {
     super();
     this.state = {
+      loading: true,
+      uploading: false,
       previewVisible: false,
       previewImage: '',
       previewTitle: '',
-      fileList: [
-        {
-          uid: '-1',
-          name: 'image.png',
-          status: 'done',
-          url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-        },
-        {
-          uid: '-2',
-          name: 'image.png',
-          status: 'done',
-          url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-        },
-      ],
+      fileList: [],
     };
   };
 
-  handleCancel = () => this.setState({ previewVisible: false });
+  componentDidMount() {
+    this.requestImageList();
+  }
 
-  handlePreview = async file => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
+  requestImageList = async () => {
+    const { success, data } = await post('/setting/list/get', {
+      imageSize: '120x120'
+    });
+    if (success) {
+      this.setState({
+        loading: false,
+        fileList: data.results,
+      })
     }
+  }
 
+  handleStatusCheck = (key) => this.setState({ [key]: !this.state[key] });
+
+  handlePreview = item => {
     this.setState({
-      previewImage: file.url || file.preview,
       previewVisible: true,
-      previewTitle: file.name || file.url.substring(file.url.lastIndexOf('/') + 1),
+      previewImage: item.url,
+      previewTitle: item.name,
     });
   };
 
-  handleChange = ({ fileList }) => this.setState({ fileList });
+  handleDeleteImage = async item => {
+    Modal.confirm({
+      title: 'Do you Want to delete these items?',
+      icon: <ExclamationCircleOutlined />,
+      onOk: async () => {
+        this.setState({
+          loading: true,
+        });
+
+        const { success } = await post('/setting/image/delete', {
+          name: item.name,
+        });
+        if (success) {
+          message.success('Delete Success');
+          this.requestImageList()  
+        } else {
+          message.error('Delete Error');
+          this.setState({
+            loading: false,
+          })
+        }
+      },
+    });
+  }
+  
+  handleBeforeUpload = (file) => {
+    this.setState({ uploading: true });
+  };
+
+  handleChange = ({event, file, fileList}) => {
+    if (file.status === 'done') {
+      message.success('Upload Success');
+      this.setState({
+        uploading: false,
+      })
+      this.requestImageList()
+    }
+  };
 
   render () {
-    const { previewVisible, previewImage, fileList, previewTitle } = this.state;
-    const uploadButton = (
-      // <div>
-        // <PlusOutlined />
-      // </div>
-        <div className="ant-upload-text">Upload</div>
-    );
+    const { previewVisible, previewImage, fileList, previewTitle, uploading } = this.state;
 
     return (
       <div>
-        loginpage-setting
-
         <Upload
-          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-          listType="picture-card"
-          fileList={fileList}
-          onPreview={this.handlePreview}
+          multiple
+          action="http://127.0.0.1:7001/setting/image/update"
+          headers={{
+            Folder: 'background',
+            Authorization: `Bearer ${JsCookies.get("AUTHORIZATION")}`,
+          }}
+          showUploadList={false}
           onChange={this.handleChange}
+          beforeUpload={this.handleBeforeUpload}
         >
-          {fileList.length >= 8 ? null : uploadButton}
+          <Button
+            loading={uploading}
+            type="primary"
+          >
+            <CloudUploadOutlined />{uploading ? 'Uploading' : 'Start Upload'}
+          </Button>
         </Upload>
+
+        <Spin spinning={this.state.loading} delay={500}>
+          <ul className={style['image_list_block']}>
+            {fileList.map((item, index) => (
+              <li className={style['image_list_item']} key={item.name}>
+                <div className={style['image_operation']}>
+                  <EyeOutlined onClick={() => this.handlePreview(item)} />
+                  <DeleteOutlined onClick={() => this.handleDeleteImage(item)} />
+                </div>
+                <img src={item.proUrl} />
+              </li>)
+            )}
+          </ul>
+        </Spin>
+
         <Modal
           visible={previewVisible}
           title={previewTitle}
-          footer={null}
-          onCancel={this.handleCancel}
+          onOk={() => {this.handleStatusCheck('previewVisible')}}
+          onCancel={() => {this.handleStatusCheck('previewVisible')}}
         >
           <img alt="example" style={{ width: '100%' }} src={previewImage} />
         </Modal>
